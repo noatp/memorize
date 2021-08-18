d155822338329043528e4f6ccecb5aa4c394097a
Toan Pham <toan.chpham@gmail.com>
2021-08-18 03:25:56 -0500

    Update ContentView.swift

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
     }
 }
 
+struct CardView: View{
+    @State var isFaceUp = true
+    
+    var body: some View{
+        ZStack{
+            let cardShape = RoundedRectangle(cornerRadius: 25.0)
+            if(isFaceUp){
+                cardShape.stroke(Color.green, lineWidth: 3)
+                cardShape.fill(Color.white)
+                Text("😀").font(.largeTitle)
+            }
+            else{
+                cardShape.fill(Color.green)
+            }
+            
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
         ContentView()
     }
 }
